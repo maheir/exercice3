e71d9b996dbe94c246d71504fa08e646f170cad0
maheir <maheirne@gmail.com>
2018-12-17 14:56:28 +0100

    VARIABLE EXERCICE 3

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercice_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string a;
+            string b;
+
+            Console.WriteLine("valeur de a");
+            a = Console.ReadLine();
+            Console.WriteLine("valeur de b");
+            b = Console.ReadLine();
+            int first = int.Parse(a);
+            int second = int.Parse(b);
+
+
+            
+
+            first = first + 33;
+            second = second + 1;
+
+            Console.WriteLine("voici le résultat " + first / second);
+
+
+
+        }
+    }
+}
